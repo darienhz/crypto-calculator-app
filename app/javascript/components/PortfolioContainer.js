74c7d1ab2c6139ed3a7d2f4a4cdc0588dc21c54f
Darien Hernandez <darienhz89@gmail.com>
2021-01-14 19:33:40 -0800

    added two new methods

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -17,6 +17,8 @@ class PortfolioContainer extends Component {
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSelect = this.handleSelect.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleAmount = this.handleAmount.bind(this)
   }
 
   handleChange(e){
@@ -47,14 +49,49 @@ class PortfolioContainer extends Component {
     })
   }
 
+  handleSubmit(e){
+    e.preventDefault()
+
+    let currency = this.state.active_currency
+    let amount = this.state.amount
+
+    axios.post('http://localhost:3000/calculate',{
+      id: currency.id,
+      amount: amount
+    })
+    .then((data) => {
+      this.setState({
+        amount: '',
+        active_currency: null,
+        portfolio: [...this.state.portfolio, data.data]
+      })
+    })
+    .catch( (data) => {
+      debugger
+    })
+
+    console.log(this.state)
+  }
+
+  handleAmount(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
 
     const searchOrCalculate = this.state.active_currency ?
-    <Calculate/> :
+    <Calculate
+      handleChange={this.handleChange}
+      handleSubmit={this.handleSubmit}
+      activeCurrency={this.state.active_currency}
+      amount={this.state.amount}
+    /> :
     <Search
-    handleSelect={this.handleSelect}
-    searchResults={this.state.search_results}
-    handleChange={this.handleChange}/>
+      handleSelect={this.handleSelect}
+      searchResults={this.state.search_results}
+      handleChange={this.handleChange}/>
 
     return(
       <div>
