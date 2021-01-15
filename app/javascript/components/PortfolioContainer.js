874747e5f204e59bc9c2a9f89b9f27913a72a70a
Darien Hernandez <darienhz89@gmail.com>
2021-01-14 17:54:32 -0800

    added some code for the search function

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -20,25 +20,29 @@ class PortfolioContainer extends Component {
   }
 
   handleChange(e){
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+    // this.setState({
+    //   [e.target.name]: e.target.value
+    // })
 
-    axios.post('https://localhost:3000/search', {
-      name: this.state.name
+    axios.post('http://localhost:3000/search', {
+      search: e.target.value
     })
     .then((data) => {
-      debugger
+      this.setState({
+        search_results: [...data.data.currencies]
+      })
     })
     .catch((data) => {
       debugger
     })
+
+    console.log(this.state.search_results)
   }
 
   render() {
     return(
       <div>
-        <Search handleChange={this.handleChange}/>
+        <Search searchResults={this.state.search_results} handleChange={this.handleChange}/>
         <Calculate/>
       </div>
     )
